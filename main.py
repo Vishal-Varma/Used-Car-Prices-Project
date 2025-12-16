7f72e1ac6cf30f2f5d938823ce6d537e54472abd
vishal <bp903206@gmail.com>
2025-12-16 17:36:49 +0530

    16/12/25

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ import joblib
 import pandas as pd
 import numpy as np
 
+# Creating the API app
 app = FastAPI()
 
+# Storing car data in list
 car_data = []
+
+#Loading the saved model
 model = joblib.load("./models/used_car_price_model.pkl")
 
+# Creating Pydantic model for car data
 class Car(BaseModel):
     brand : str
     model : str
@@ -22,21 +27,30 @@ class Car(BaseModel):
     seats : int
     vehicle_age : int
 
+# API Endpoints
+
+# Root endpoint
 @app.get("/")
 def read_root():
     return {"message" : "Welcome to the Used Car Prices Prediction API!"}
 
+# Add car data endpoint
+# Used to add car data for prediction
 @app.post("/add_car/")
 def add_car(car : Car):
     car_data.append(car)
     return {"message" : "Car data added successfully", "car" : car}
 
+# Get car data endpoint
+# Used to get car data by index
 @app.get("/cars/{car_id}/", response_model = Car)
 def get_car(car_id : int):
     if car_id < 0 or car_id >= len(car_data):
         raise HTTPException(status_code = 404, detail = "Car not found")
     return car_data[car_id]
 
+# Predict price endpoint
+# Used to predict price of the car based on the provided data
 @app.post("/predict_price/")
 def get_prediction():
     if not car_data:
